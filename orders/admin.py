671b94f531d6f906e08e5e8bd833ffabc00d942d
Oscar <NAge@email.com>
2019-02-14 17:05:31 -0600

    Crate addtional ORM:User_Profile and mino mods to exisiting Models

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import PizzaOrder, PizzaPrice, Toppings
+from .models import PizzaOrder, PizzaPrice, Toppings,UserProfile
 
 # Register your models here.
 admin.site.register(PizzaOrder)
 admin.site.register(PizzaPrice)
 admin.site.register(Toppings)
+admin.site.register(UserProfile)
--- orders/models.py
+++ orders/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import signals
+
+
 
 
 # Create your models here.
 class PizzaPrice(models.Model):
     classification = models.CharField(max_length=10)
     size = models.CharField(max_length=10)
-    num_toppings = models.IntegerField()
-    price = models.IntegerField()
+    num_toppings = models.IntegerField( default=0)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
         verbose_name_plural = "Pizza Price"
@@ -18,12 +21,25 @@ class Toppings(models.Model):
 
     class Meta:
         verbose_name_plural = "Toppings"
+        
 
 
-class PizzaOrder(models.Model):
-    author = models.ForeignKey(User, related_name="orders", on_delete=models.CASCADE)
-    topping = models.ManyToManyField(Toppings)
-    price = models.ForeignKey(PizzaPrice, on_delete=models.Case, related_name="orders")
+class PizzaOrder(models.Model): 
+    author = models.ForeignKey('UserProfile', related_name="orders", on_delete=models.CASCADE)
+    topping = models.ManyToManyField(Toppings, blank=True)
+    price = models.ForeignKey(PizzaPrice, on_delete=models.CASCADE, related_name="orders")
 
     class Meta:
         verbose_name_plural = "Pizza Order"
+
+class UserProfile(models.Model):
+    author=models.OneToOneField(User,on_delete=models.CASCADE)
+    #a user can have more than one Pizza Order, hence the many-to-many realtionship
+    pizza_order=models.ManyToManyField(PizzaOrder, blank=True)
+
+#create a user profile
+def create_profile(sender, instance, created, **kwargs):  
+    if created:
+        UserProfile.objects.create(author=instance)
+#if a user is created, create a user profile automatically 
+signals.post_save.connect(create_profile, sender=User, weak=False, dispatch_uid='models.create_profile')
